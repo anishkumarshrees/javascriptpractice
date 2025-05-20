2157ca237c2520b718fb34f3fec17ea0e2555732
anishkumarshrees <anishshrees12@gmail.com>
2025-05-20 12:19:42 +0545

    first commit

---
+++ day2.js
@@ -0,0 +1,14 @@
+//node --watch day2.js - this cmd is to run code automatically everytime when you save file
+
+//variable
+
+
+const hello="k xa"
+const cup="k xa khbr"
+const isNepali=true
+let age=23
+
+console.log(cup)
+console.log(cup + " thik xa")
+console.log(hello + `chiya xa`)
+console.log(`my age is ${age} and i am nepali`)
