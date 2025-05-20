5f90022b49ad6e5c4c335e15abc23353469e4bed
anishkumarshrees <anishshrees12@gmail.com>
2025-05-20 13:15:00 +0545

    day3

--- day2.js
+++ day2.js
@@ -1,6 +1,8 @@
 //node --watch day2.js - this cmd is to run code automatically everytime when you save file
 
 //variable
+//primitive datatype= string, number, boolean
+//non primitive datatype = array, object
 
 
 const hello="k xa"
---
+++ day3.js
@@ -0,0 +1,35 @@
+//array-- if same types of data then use array
+
+// let shoe1="goldstar" this is variable it consume more time
+// let shoe2="nike"
+// let shoe3="adidas"
+let shoes=["goldsta","addida","nike"] //this is array if there are many data in same types we used array
+shoes.push("gucci")
+//methods in array
+//shoes.push()add to last
+//shoes.pop()//remove last
+//shoes.shift()//remove first
+//shoes.unshift()//add first
+console.log(shoes)
+let hello=[{
+    name:"anish"
+},
+{
+name:"shrees"
+}
+]
+console.log(hello)
+
+
+//object -- if many data specify same information we use object
+
+let name="anish"
+let age=23
+let address="Butwal"
+
+let myInformation = {
+    name:"anish",
+    age:23,
+    address:["Butwal","gulmi"]
+}
+console.log(myInformation)
